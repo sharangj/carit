84968835cfa4840717e9685cfe8edf0ca6232b7b
Ramsharan <sharangj@gmail.com>
2012-12-09 15:22:18 +0100

    After Home page creation

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Carpool::Application.routes.draw do
   root to: "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  # match '/' => 'devise/sessions#new'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
